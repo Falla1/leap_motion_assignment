6e93aaa153d8031fdba4aa8766f0ed28907ffa8a
SamuSan <sambminns@gmail.com>
2015-07-28 12:12:35 +1200

    Refactor logger to be DRY

--- js/LeapLogger.js
+++ js/LeapLogger.js
@@ -10,12 +10,16 @@ function LeapLogger () {
       outputContent += '<br> Has the class we\'re looking for ' + (el.id === 'load-button');
     }
 
-    if(hands['l']) outputContent += '<br> Are we grabbing shit with our lefthand?' + hands['l'].grabStrength;
-    if(hands['r']) outputContent += '<br> Are we grabbing shit with our righthand?' + hands['r'].grabStrength;
-    if(hands['l']) outputContent += '<br> Are we pinching shit?' + hands['l'].pinchStrength;
-    if(hands['r']) outputContent += '<br> Are we pinching shit?' + hands['r'].pinchStrength;
-    if(hands['l']) outputContent += '<br> They see me rolling left'   + hands['l'].roll();
-    if(hands['r']) outputContent += '<br> They see me rolling right'   + hands['r'].roll();
+    if(hands['l']){
+      outputContent += '<br> Are we grabbing shit with our lefthand?' + hands['l'].grabStrength;
+      outputContent += '<br> Are we pinching shit?' + hands['l'].pinchStrength;
+      outputContent += '<br> They see me rolling left'   + hands['l'].roll();
+    }
+    if(hands['r']) {
+      outputContent += '<br> Are we grabbing shit with our righthand?' + hands['r'].grabStrength;
+      outputContent += '<br> Are we pinching shit?' + hands['r'].pinchStrength;
+      outputContent += '<br> They see me rolling right'   + hands['r'].roll();
+    }
   };
 
   self.updateLogOutput = function(data) {
